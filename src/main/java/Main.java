c3de3264f93231d8a137a13fd53456e03295ec17
Alina Burnos <burnosalina@gmail.com>
2023-03-14 08:39:13 +0100

    Kody krajów

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
@@ -10,7 +15,37 @@ public class Main {
     }
 
     void run(Scanner scanner) {
-        // usupełnij metodę
+        String fileName = "countries.csv";
+        try {
+            Map<String, Country> countryMap = readFile(fileName);
+            System.out.println("Podaj kod kraju, o którym chcesz zobaczyć informacje:");
+            String code = scanner.nextLine();
+            printInfo(code, countryMap);
+        } catch (IOException e) {
+            System.out.println("Brak pliku " + fileName + ".");
+        }
     }
 
+    private Map<String, Country> readFile(String fileName) throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(fileName));
+        Map<String, Country> countryMap = new HashMap<>();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            String[] split = line.split(";");
+            String code = split[0];
+            String name = split[1];
+            int population = Integer.parseInt(split[2]);
+            countryMap.put(code, new Country(code, name, population));
+        }
+        return countryMap;
+    }
+
+    private void printInfo(String code, Map<String, Country> countryMap) {
+        if (countryMap.containsKey(code)) {
+            Country country = countryMap.get(code);
+            System.out.println(country);
+        } else {
+            System.out.println("Kod kraju " + code + " nie został znaleziony.");
+        }
+    }
 }
